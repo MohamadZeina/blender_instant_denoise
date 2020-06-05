d2eebc38b1ee413e9d7acad458aeb2611b2efe94
Mohamad Zeina <mohamadzeina@gmail.com>
2020-06-05 18:07:27 +0100

    Built basic UI and testing scene

---
+++ blender_instant_denoise.py
@@ -0,0 +1,72 @@
+bl_info = {
+    "name": "Blender Instant Denoise. By Moby Motion",
+    "blender": (2, 82, 0),
+    "category": "Object",
+    "description": "Apply the new Intel denoiser in a single click.",
+    "author": "Moby Motion",
+    "version": (0, 0, 1),
+    "location": "",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": ""
+}
+
+# Imports
+import bpy
+
+# Classes
+class InstantDenoise(bpy.types.Operator):
+    """Apply the new Intel denoiser in a single click."""
+    bl_idname = "object.instantdenoise"
+    bl_label = "Denoise"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        """Called when blender runs this operator"""
+
+        scene = context.scene
+
+        return {'FINISHED'}
+
+
+class InstantDenoisePanel(bpy.types.Panel):
+    """The primary panel for Blender AutoFocus"""
+    bl_label = "Instant AI denoise"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "render"
+
+    def draw(self, context):
+        layout = self.layout
+        obj = context.object
+        scene = context.scene
+        
+        row = layout.row()
+        row.label(text="Click to apply Intel AI denoising", icon='CAMERA_DATA')
+
+        row = layout.row()
+        row.operator("object.instantdenoise")
+
+
+# Register classes
+classes = (
+    InstantDenoisePanel,
+    InstantDenoise,
+)
+
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+    
+
+def unregister():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+
+# Allows script to run directly from Blender's Text editor
+if __name__ == "__main__":
+    register()
+    
